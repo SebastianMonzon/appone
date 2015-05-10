cb341effb7bb04c5809d803a2ce82b54bfabba51
Sebastian Monzon <sbr.monzon@gmail.com>
2015-05-10 00:47:11 -0400

    finished authorization

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150506212736) do
+ActiveRecord::Schema.define(version: 20150510035543) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -27,7 +27,10 @@ ActiveRecord::Schema.define(version: 20150506212736) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -47,6 +50,7 @@ ActiveRecord::Schema.define(version: 20150506212736) do
     t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
